50a37b3c7096d8884c5cc59c4d5c435312a4376c
P A Adithyan <adithyananil19@gmail.com>
2026-09-18 11:37:53 +0530

    modified

--- practical2/app.py
+++ practical2/app.py
@@ -4,7 +4,7 @@ import joblib
 from pathlib import Path
 
 # Load saved pipeline
-model_path = Path(__file__).parent / "electric_bill_ac_model.pkl"
+model_path = Path(__file__).parent / "electric_bill_ac_fan_model.pkl"
 model = joblib.load(model_path)
 
 # App title
@@ -12,17 +12,21 @@ st.title("Electricity Bill Predictor")
 st.write("Enter the number of units consumed by AC to predict the electricity bill.")
 
 # User input
-units_consumed = st.number_input(
+units_ac = st.number_input(
     "Units consumed by AC",
     min_value=10.0,
     step=1.0
 )
-
+units_fan = st.number_input(
+    "Units consumed by Fan",
+    min_value=10.0,
+    step=1.0
+)
 # Prediction
 if st.button("Predict"):
 
     input_data = pd.DataFrame({
-        "AC_Units": [units_consumed]
+        "AC_Units": [units_ac],"Fan_Units": [units_fan]
     })
 
     prediction = model.predict(input_data)[0]
